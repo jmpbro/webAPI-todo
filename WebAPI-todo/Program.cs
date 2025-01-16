e7711bc8dd26c5ac40e3f6cda947b9460c8713b2
Jolene <jolene.m.p.brown@gmail.com>
2025-01-16 10:21:40 +0000

    Added endpoints and tested with JSON data

--- WebAPI-todo/Program.cs
+++ WebAPI-todo/Program.cs
@@ -1,6 +1,36 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "This is going to call the list of todos that are in the database");
+var todos = new List<Todo>();
+
+// get ALL
+app.MapGet("/todos", () => todos); // mappget ALL method and handler
+
+// get by ID
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) => // URL string id and parameter
+{
+    var targetTodo = todos.SingleOrDefault(t => id ==t.Id);
+    return targetTodo is null
+    ? TypedResults.NotFound()
+    : TypedResults.Ok(targetTodo);
+});
+
+// post
+app.MapPost("/todos", (Todo task) => // mappost method and handler
+{
+    todos.Add(task);
+    return TypedResults.Created("/todos/{id}", task); // outputs status code 201 (created)
+});
+
+// delete
+app.MapDelete("/todos/{id}", (int id) =>
+{
+    todos.RemoveAll(t => id == t.Id);
+    return TypedResults.NoContent();
+});
 
 app.Run();
+
+public record Todo(int Id, string Name, DateTime DueDate, bool inCompleted); // record type
